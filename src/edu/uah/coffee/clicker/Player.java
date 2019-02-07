4d1aa6d4cc7d32c735a646a742f980fa294aa11c
Brandenburg <Brandenburg@10.4.145.170>
2019-02-07 16:52:57 -0600

    Added Player Name and Coffee Shop Name strings for user input

--- src/edu/uah/coffee/clicker/Player.java
+++ src/edu/uah/coffee/clicker/Player.java
@@ -1,5 +1,10 @@
 package edu.uah.coffee.clicker;
+import java.util.Scanner;
 
 public class Player {
-	
+        Scanner CoffeShop = new Scanner(System.in);
+        Scanner PlayerName = new Scanner(System.in);
+        
+        double CoffePerSec = 0;
+        
 }
