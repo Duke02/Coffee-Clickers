05692414de208a395f533899e0c65131c90140ea
Brandenburg <Brandenburg@10.4.145.170>
2019-03-06 10:11:35 -0600

    Did a few changes and added some JAR files

--- src/main/java/edu/uah/coffee/clicker/ReadJSONFile.java
+++ src/main/java/edu/uah/coffee/clicker/ReadJSONFile.java
@@ -4,16 +4,12 @@ package main.java.edu.uah.coffee.clicker;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
+//import java.text.ParseException;
 import java.util.Iterator;
-import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.parser.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/**
- *
- * @author Brandenburg
- */
 public class ReadJSONFile {
 
     public static void main(String[] args){
@@ -22,17 +18,17 @@ public class ReadJSONFile {
         
         try
         {
-            Object obj = parser.parse(new FileReader("Coffee_json.json"));
+            Object obj = parser.parse(new FileReader("CoffeeJSON.json"));
             JSONObject jsonObject = (JSONObject) obj;
             String name = (String) jsonObject.getString("name");
             System.out.println("Name is: "+ name);
             
-            String location = (String) jsonObject.getString("location");
-            System.out.println("Loaction is: " + location);
+            String CPS = (String) jsonObject.getString("CPS");
+            System.out.println("CPS is: " + CPS);
             
             //loop array
             JSONArray BuildingsArray = (JSONArray) jsonObject.getJSONArray("Buildings");
-            Iterator<String> iterator = BuildingsArray.iterator();
+            Iterator<Object> iterator = BuildingsArray.iterator();
             
             while(iterator.hasNext())
             {
@@ -41,7 +37,7 @@ public class ReadJSONFile {
         }
         catch(FileNotFoundException e) { e.printStackTrace(); }
         catch(IOException e) { e.printStackTrace(); }
-        catch(ParseException e) { e.printStackTrace(); }
+        //catch(ParseException e) { e.printStackTrace(); }
         catch(Exception e) { e.printStackTrace(); }
         
     }
