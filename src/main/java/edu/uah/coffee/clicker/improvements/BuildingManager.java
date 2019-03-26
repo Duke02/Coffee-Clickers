bf2a861ef3814e4d2e36173709ad29933b6618a9
Branden Noack <bmn0006@uah.edu>
2019-03-25 19:17:50 -0500

    Merge branch 'master' of https://github.com/Duke02/Coffee-Clickers
    # Conflicts:
    #	pom.xml

--- src/main/java/edu/uah/coffee/clicker/improvements/BuildingManager.java
+++ src/main/java/edu/uah/coffee/clicker/improvements/BuildingManager.java
@@ -5,5 +5,151 @@ package edu.uah.coffee.clicker.improvements;
  * @author trystanmay
  */
 public class BuildingManager extends AbstractManager {
-    
+   
+
+private int Building1CPS;
+private int Building2CPS;
+private int Building3CPS;
+private int Building4CPS;
+private int Building5CPS;
+private int Building6CPS;
+
+private int Building1ID;
+private int Building2ID;
+private int Building3ID;
+private int Building4ID;
+private int Building5ID;
+private int Building6ID;
+
+private String Building1Name;
+private String Building2Name;
+private String Building3Name;
+private String Building4Name;
+private String Building5Name;
+private String Building6Name;
+
+
+public void setBuilding1CPS(int Building1CPS) {
+  this.Building1CPS = Building1CPS;
+ }
+ public int getBuilding1CPS() {
+  return Building1CPS;
+ }
+ 
+ public void setBuilding1ID(int Building1ID)  {
+     this.Building1ID = Building1ID;
+ }
+ public int getBuilding1ID()  {
+     return Building1ID;
+ }
+
+
+public void setBuilding1Name(String Building1Name) {
+  this.Building1Name = Building1Name;
+ }
+ public String getBuilding1Name() {
+  return Building1Name;
+ }
+ 
+ public void setBuilding2CPS(int Building2CPS) {
+  this.Building2CPS = Building2CPS;
+ }
+ public int getBuilding2CPS() {
+  return Building2CPS;
+ }
+  public void setBuilding2ID(int Building2ID)  {
+     this.Building2ID = Building2ID;
+ }
+ public int getBuilding2ID()  {
+     return Building2ID;
+ }
+
+public void setBuilding2Name(String Building2Name) {
+  this.Building2Name = Building2Name;
+ }
+ public String getBuilding2Name() {
+  return Building2Name;
+ }
+ 
+ 
+ public void setBuilding3CPS(int Building3CPS) {
+  this.Building3CPS = Building3CPS;
+ }
+ public int getBuilding3CPS() {
+  return Building3CPS;
+ }
+ public void setBuilding3ID(int Building3ID)  {
+     this.Building3ID = Building3ID;
+ }
+ public int getBuilding3ID()  {
+     return Building3ID;
+ }
+ public void setBuilding3Name(String Building3Name) {
+  this.Building3Name = Building3Name;
+ }
+ public String getBuilding3Name() {
+  return Building3Name;
+ }
+
+
+public void setBuilding4Name(String Building4Name) {
+  this.Building4Name = Building4Name;
+ }
+ public String getBuilding4Name() {
+  return Building4Name;
+ }
+ 
+ public void setBuilding4CPS(int Building4CPS) {
+  this.Building4CPS = Building4CPS;
+ }
+ public int getBuilding4CPS() {
+  return Building4CPS;
+ }
+  public void setBuilding4ID(int Building4ID)  {
+     this.Building4ID = Building4ID;
+ }
+ public int getBuilding4ID()  {
+     return Building4ID;
+ }
+
+ 
+ public void setBuilding5CPS(int Building5CPS) {
+  this.Building5CPS = Building5CPS;
+ }
+ public int getBuilding5CPS() {
+  return Building5CPS;
+ }
+  public void setBuilding5ID(int Building5ID)  {
+     this.Building5ID = Building5ID;
+ }
+ public int getBuilding5ID()  {
+     return Building5ID;
+ }
+public void setBuilding5Name(String Building5Name) {
+  this.Building5Name = Building5Name;
+ }
+ public String getBuilding5Name() {
+  return Building5Name;
+ }
+ 
+ public void setBuilding6Name(String Building6Name) {
+  this.Building6Name = Building6Name;
+ }
+ public String getBuilding6Name() {
+  return Building6Name;
+ }
+ 
+ public void setBuilding6CPS(int Building6CPS) {
+  this.Building6CPS = Building6CPS;
+ }
+ public int getBuilding6CPS() {
+  return Building6CPS;
+ }
+  public void setBuilding6ID(int Building6ID)  {
+     this.Building6ID = Building6ID;
+ }
+ public int getBuilding6ID()  {
+     return Building6ID;
+ }
 }
+
