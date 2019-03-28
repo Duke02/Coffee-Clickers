1c0ac8e9aac54c85d4fad061b9b27c0e4749935d
Trystan May <trystanmay@gmail.com>
2019-03-28 17:39:33 -0500

    Changed Upgrade
    Signed-off-by: Trystan May <trystanmay@gmail.com>

--- src/main/java/edu/uah/coffee/clicker/improvements/Upgrade.java
+++ src/main/java/edu/uah/coffee/clicker/improvements/Upgrade.java
@@ -1,9 +1,23 @@
 package edu.uah.coffee.clicker.improvements;
 
 /**
- *
  * @author trystanmay
  */
 public class Upgrade implements Improvement {
-    
+
+	private int id;
+	private String name;
+
+
+	public int getId () {
+		return this.id;
+	}
+
+	public String getName () {
+		return this.name;
+	}
+
+	public void setName ( String name ) {
+		this.name = name;
+	}
 }
