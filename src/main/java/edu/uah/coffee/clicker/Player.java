e17d5f7d89eedf545fb63afa7abcbc8479568e74
Trystan May <trystanmay@gmail.com>
2019-04-11 17:16:25 -0500

    Added remaining non-view JavaDocs.
    Signed-off-by: Trystan May <trystanmay@gmail.com>

--- src/main/java/edu/uah/coffee/clicker/Player.java
+++ src/main/java/edu/uah/coffee/clicker/Player.java
@@ -5,25 +5,39 @@ public class Player extends CoffeeClickerModel {
 
 	int numberOfCoffeeBeans;
 
+	/**
+	 * Creates a new player model with the model name provided by Constants.
+	 *
+	 * @see Constants#PLAYER_MODEL_NAME
+	 */
 	public Player () {
 		super( Constants.PLAYER_MODEL_NAME );
 	}
 
+	/**
+	 * @return the number of acquired coffee beans.
+	 */
 	public int getCoffeeBeans () {
 		return numberOfCoffeeBeans;
 	}
 
-	public void setCoffeeBeans ( int numberOfCoffeeBeans ) {
-		this.numberOfCoffeeBeans = numberOfCoffeeBeans;
-		this.setChanged();
-	}
-
+	/**
+	 * Adds the specified number of coffee beans to the mound the player has amassed.
+	 *
+	 * @param i the amount to add.
+	 */
 	public void addCoffeeBeans ( int i ) {
 		this.numberOfCoffeeBeans += i;
 		this.setChanged();
 		this.notifyObservers();
 	}
 
+	/**
+	 * Removes beans from the player's stash (usually in a purchase).
+	 *
+	 * @param cost the amount of beans to lose.
+	 * @throws IllegalArgumentException if the number of beans the player has is less than the cost.
+	 */
 	public void loseBeans ( int cost ) throws IllegalArgumentException {
 		if ( this.numberOfCoffeeBeans < cost ) {
 			throw new IllegalArgumentException( "Cannot lost more beans than currently have." );
--- src/main/java/edu/uah/coffee/clicker/ResourceManager.java
+++ src/main/java/edu/uah/coffee/clicker/ResourceManager.java
@@ -4,6 +4,12 @@ import java.io.*;
 
 public class ResourceManager {
 
+	/**
+	 * Gets an InputStreamReader for the desired filename.
+	 *
+	 * @param filename a filepath in the format of /path/to/file.ext
+	 * @return an InputStreamReader at the given path.
+	 */
 	public static Reader getReader ( String filename ) {
 		InputStream is = ResourceManager.class.getResourceAsStream( filename );
 		try {
@@ -18,6 +24,12 @@ public class ResourceManager {
 		}
 	}
 
+	/**
+	 * Gets a file at the desired path.
+	 *
+	 * @param filename A path to the desired file with the format of /path/to/file.ext
+	 * @return a File object representing the stored file.
+	 */
 	public static File getFile ( String filename ) {
 		String path = ResourceManager.class.getResource( filename ).getFile();
 		return new File( path );
