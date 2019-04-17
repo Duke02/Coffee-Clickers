ab22cff6d9376fcfa2ff7fbb786eafc05f6f79e5
Branden Noack <bmn0006@uah.edu>
2019-04-17 16:22:01 -0500

    Merge branch 'master' of https://github.com/Duke02/Coffee-Clickers
    # Conflicts:
    #	pom.xml

--- src/main/java/edu/uah/coffee/clicker/ResourceManager.java
+++ src/main/java/edu/uah/coffee/clicker/ResourceManager.java
@@ -3,11 +3,16 @@ package edu.uah.coffee.clicker;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.Serializable;
 
-public class ResourceManager {
+public class ResourceManager  implements Serializable{
 
 	public static Reader getReader ( String filename ) {
 		InputStream is = ResourceManager.class.getResourceAsStream( filename );
 		return new InputStreamReader( is );
 	}
+
+    void WriteObjectToFile(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
