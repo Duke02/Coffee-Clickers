111be3f28d67f9ca42a5d2ab941da211c8d20f8f
Trystan May <trystanmay@gmail.com>
2019-04-20 13:04:22 -0500

    Revert "Added more saving."
    This reverts commit 80048df4b80d6a3a08b308d68dbeaebce43b4984.
    
    # Conflicts:
    #	src/main/java/edu/uah/coffee/clicker/ResourceManager.java
    #	src/main/java/edu/uah/coffee/clicker/controller/SaveGameController.java

--- src/main/java/edu/uah/coffee/clicker/ResourceManager.java
+++ src/main/java/edu/uah/coffee/clicker/ResourceManager.java
@@ -1,6 +1,13 @@
 package edu.uah.coffee.clicker;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Reader;
+import java.io.Serializable;
 
 public class ResourceManager {
 
@@ -23,6 +30,40 @@ public class ResourceManager {
 			return null;
 		}
 	}
+	public void WriteObjectToFile(String filepath,CoffeeClickerModel model) {
+
+		try {
+
+			FileOutputStream fileOut = new FileOutputStream(filepath);
+			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+			objectOut.writeObject(model);
+			objectOut.close();
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Object ReadObjectFromFile(String filepath) {
+
+		try {
+
+			FileInputStream fileIn = new FileInputStream(filepath);
+			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+
+			Object obj = objectIn.readObject();
+
+			objectIn.close();
+			return obj;
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+}
 
 	/**
 	 * Gets a file at the desired path.
---
+++ src/main/java/edu/uah/coffee/clicker/controller/SaveGameController.java
@@ -0,0 +1,44 @@
+package edu.uah.coffee.clicker.controller;
+
+import edu.uah.coffee.clicker.CoffeeClickerModel;
+import edu.uah.coffee.clicker.Constants;
+import edu.uah.coffee.clicker.ResourceManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.util.Observable;
+
+public class SaveGameController extends Controller implements ActionListener {
+
+	public SaveGameController () {
+		super( Constants.SAVE_GAME_CONTROLLER_NAME );
+	}
+
+	@Override
+	public void update ( Observable o, Object arg ) {
+		if ( ! ( o instanceof CoffeeClickerModel ) ) {
+			return;
+		}
+
+
+	}
+
+	@Override
+	public void propertyChange ( PropertyChangeEvent evt ) {
+
+	}
+
+	private void saveGame ( String filepath ) {
+		for ( CoffeeClickerModel model : this.models.values() ) {
+			ResourceManager.writeObjectToFile( model, filepath );
+		}
+	}
+
+	@Override
+	public void actionPerformed ( ActionEvent e ) {
+		if ( e.getActionCommand().equals( "saveGame" ) ) {
+			saveGame( "coffee-clickers.dat" );
+		}
+	}
+}
